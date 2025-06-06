5aea204bd9447716b77e8c9905776cdb0b9bdc76
Thiveran <thiveran1420@gmail.com>
2025-06-06 11:46:34 +0530

    searinds

--- blocks/searchengine/searchengine.js
+++ blocks/searchengine/searchengine.js
@@ -1,10 +1,13 @@
 export default function decorate(block) {
-    
+    // Create label
+    const label = document.createElement('p');
+    label.textContent = 'Enter the page to search';
+ 
     // Create input
     const input = document.createElement('input');
     input.type = 'text';
     input.id = 'searchInput';
-    input.placeholder = 'Enter title or tag to search a page';
+    input.placeholder = 'Enter title or tag';
  
     // Create button
     const button = document.createElement('button');
